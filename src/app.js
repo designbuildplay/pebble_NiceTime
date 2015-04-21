36b74e6a20ed3bf42a21abbcfd623464ee6f3770
Josh Freeman <info@designbuildplay.co.uk>
2015-04-21 11:22:06 +0100

    position amends

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ var Vector2 = require('vector2');
 var main = new UI.Window();
 
 var textfield = new UI.Text({
-    position: new Vector2(10, 50),
+    position: new Vector2(10, 115),
     size: new Vector2(144, 30),
     font: 'gothic-36-bold',
     text: 'HAVE A NICE DAY',
@@ -30,8 +30,10 @@ var textfield = new UI.Text({
 main.add(textfield);
 
 // Create the smile image.
-var smile = new Image({
-  image:'images/smile.png',
+var smile = new UI.Image({
+  position: new Vector2(30, 20),
+  size: new Vector2(84, 84),
+  image:'images/smile_84.png',
   compositing:'normal'
 });
 main.add(smile);
